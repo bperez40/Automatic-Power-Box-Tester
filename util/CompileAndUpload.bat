676c81d57099266316b514cfb4a08a72b6f045b3
bperez40 <bperez40@gatech.edu>
2022-08-09 11:03:53 -0400

    Adjusted CompileAndUpload script

--- util/CompileAndUpload.bat
+++ util/CompileAndUpload.bat
@@ -1,2 +1,4 @@
-cd ..
-arduino-cli.exe compile --build-path ./build --libraries ./lib -b arduino:avr:mega -p COM4 -u main/main.ino
+@ECHO OFF
+git submodule update --init --recursive
+cd ../.arduino-cli
+arduino-cli.exe compile --build-path ../build --libraries ../lib -b arduino:avr:mega -p COM4 -u ../main/main.ino
